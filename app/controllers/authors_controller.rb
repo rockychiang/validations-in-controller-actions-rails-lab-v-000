595175bbb697514d96a428f374aa01dc21f762a6
Rocky Chiang <rocky@chem.ucla.edu>
2018-04-16 20:26:57 +0000

    automatically backed up by learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -11,8 +11,9 @@ class AuthorsController < ApplicationController
     if @author.valid?
       @author.save
       redirect_to author_path(@author)
-    else 
+    else
       render new_author_path
+    end
   end
 
   private
